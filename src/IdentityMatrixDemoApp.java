159bf37afda297ef4b19016d483166d266b07f4d
skochauf <stephan.kochauf@lv.campus02.at>
2022-10-08 11:19:41 +0200

    2-dim array

--- src/IdentityMatrixDemoApp.java
+++ src/IdentityMatrixDemoApp.java
@@ -1,13 +1,24 @@
 public class IdentityMatrixDemoApp {
     public static void main(String[] args) {
 
-        int[][] matrix = generateMatrix(5);
+        int size = 5;
+        int[][] matrix = generateMatrix(size);
+
+        for (int x = 0; x < matrix.length; x++) {
+            for (int y = 0; y < matrix[x].length;y++) {
+                System.out.print(matrix[x][y] + " | ");
+            }
+            System.out.println();
+        }
     }
 
     public static int[][] generateMatrix(int size){
         int[][] matrix = new int[size][size];
 
         // your code
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = 1;
+        }
 
         return matrix;
     }
